1e6f793e3e4a965f02053faef4775d71571c781d
nizarade <nizaradekurniawan@gmail.com>
2025-02-11 16:47:41 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ const App = () => {
         <img src={assets.logo} alt="" />
       </div>
 
-      <div className="flex flex-col items-center  justify-center md:flex-row grid-cols-2 gap-2 my-2 lg:my-24">
-        <div className="lg:max-w-2xl md:max-w-lg mx-auto my-auto p-4 ">
+      <div className="flex flex-col items-center   justify-center md:flex-row grid-cols-2 gap-2 md:py-10 ">
+        <div className=" md:max-w-lg lg:max-w-xl mx-auto my-auto p-4 ">
           <CarouselTransition autoslide={true} autoSlideInterval={3000}>
             {slides.map((s) => (
               <img src={s} alt="" />
@@ -20,7 +20,7 @@ const App = () => {
           </CarouselTransition>
         </div>
 
-        <div className=" flex flex-col items-center  md:w-2xl justify-center w-full mx-auto  ">
+        <div className=" flex flex-col items-center  md:w-2xl justify-center w-full mx-auto p-11 ">
           <img className="mb-2 w-48 md:w-md  " src={assets.logo_big} alt="" />
           <div className="flex flex-col items-center py-6 bg-[#D3DAFF] w-full rounded-xl shadow-2xl">
             <p className="text-2xl font-bold my-5">MCC Hotspot Login</p>
