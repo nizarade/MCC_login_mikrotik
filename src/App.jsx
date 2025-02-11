814c6a39c4d7368c16684c34e0dcf9b46fd067dd
nizarade <nizaradekurniawan@gmail.com>
2025-02-11 16:36:37 +0700

    Penyempurnaan Design
    Proses penyempurnaan design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { assets } from "./assets/assets.js";
+import CarouselTransition from "./CarouselTransition.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const slides = [assets.adds, assets.adds, assets.adds];
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-[url(./assets/bg_vertical.png)]  bg-cover bg-center w-screen h-full md:bg-[url(./assets/bg_horizontal.png)] lg:h-screen overflow-hidden">
+      <div className="bg-sky-900 py-2 px-2">
+        <img src={assets.logo} alt="" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className="flex flex-col items-center  justify-center md:flex-row grid-cols-2 gap-2 my-2 lg:my-24">
+        <div className="lg:max-w-2xl md:max-w-lg mx-auto my-auto p-4 ">
+          <CarouselTransition autoslide={true} autoSlideInterval={3000}>
+            {slides.map((s) => (
+              <img src={s} alt="" />
+            ))}
+          </CarouselTransition>
+        </div>
+
+        <div className=" flex flex-col items-center  md:w-2xl justify-center w-full mx-auto  ">
+          <img className="mb-2 w-48 md:w-md  " src={assets.logo_big} alt="" />
+          <div className="flex flex-col items-center py-6 bg-[#D3DAFF] w-full rounded-xl shadow-2xl">
+            <p className="text-2xl font-bold my-5">MCC Hotspot Login</p>
+            <input
+              type="password"
+              placeholder="Password"
+              className=" bg-white drop-shadow-lg border-sky-900 w-3/4 py-2 px-4 rounded-4xl"
+            />
+            <button className=" bg-[#001A72] drop-shadow-lg rounded-4xl py-2 px-8 my-4 text-white text-lg font-semibold">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/assets.js
@@ -0,0 +1,7 @@
+import logo from "./logo.svg";
+import logo_big from "./logo_big.svg";
+import bg_vertical from "./bg_vertical.png";
+import bg_horizontal from "./bg_horizontal.png";
+import adds from "./adds.png";
+
+export const assets = { logo, logo_big, bg_vertical, bg_horizontal, adds };
